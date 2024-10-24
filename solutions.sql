97d615afcfa75df9ae7e720aa03b5a4f5ff048c9
Mahfuz <aamahfuz.pro@gmail.com>
2024-10-24 16:00:57 +0600

    CREATE THE ENROLLMENT TABLE!

--- solutions.sql
+++ solutions.sql
@@ -1,6 +1,6 @@
 -- Active: 1729760896279@@127.0.0.1@5432@edu_institute_db
 
---- CREATE A STUDENTS TABLE! ---
+--- CREATE A STUDENTS TABLE!
 CREATE TABLE students (
     student_id SERIAL PRIMARY KEY,   -- Unique identifier for students
     student_name VARCHAR(100),       -- Student's name
@@ -11,9 +11,17 @@ CREATE TABLE students (
     status VARCHAR(50)               -- Result status of the student
 );
 
--- CREATE THE COURSES TABLE! --
+-- CREATE THE COURSES TABLE!
 CREATE TABLE courses (
     course_id SERIAL PRIMARY KEY,     -- Unique identifier for courses
     course_name VARCHAR(100),         -- Course's name
     credits INTEGER                   -- Number of credits for the course
+);
+
+
+-- CREATE THE ENROLLMENT TABLE!
+CREATE TABLE enrollment (
+    enrollment_id SERIAL PRIMARY KEY,  -- Unique identifier for enrollments
+    student_id INTEGER REFERENCES students(student_id),  -- Foreign key referencing students
+    course_id INTEGER REFERENCES courses(course_id)     -- Foreign key referencing courses
 );
